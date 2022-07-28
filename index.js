3f08d5edf9eceec088556630fc5f6b464359bcdd
Rohit Banik <rohitbanik.kolkata@gmail.com>
2022-07-28 12:11:08 +0530

    Completed by Rohit Banik

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
 class SortedList {
   constructor() {
-  
+    this.items = [],
+    this.length = []
   }
-  add() {
-   
+  add(item) {
+    var newitem = this.items;
+    newitem.push(item);
+    newitem.sort((a,b) => a>b ? 1 : -1)
+    this.items = newitem;
+    this.length = this.items.length;
   }
-  get() {
-
+  get(pos) {
+    if(pos>this.length-1){
+      throw new Error('OutOfBounds');
+    }
+    else{
+      return this.items[pos];
+    }
   }
   max() {
-
+    if(this.length==0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.max(...this.items);
+    }
   }
   min() {
-
+    if(this.length==0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.min(...this.items);
+    }
   }
   avg() {
-    
-   
+    if(this.length==0){
+      throw new Error('EmptySortedList');
+    }
+    return this.sum()/this.length;   
   }
 
   sum() {
-  
+    var sum = this.items.reduce((total, val) => {
+      return total+val
+    }, 0)
+    return sum
   }
-  
 };
 
 module.exports = SortedList;
